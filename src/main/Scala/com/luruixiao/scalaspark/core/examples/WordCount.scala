d9fcc5a0c0b0059022ae9d47704b7b79a8485087
luruixiao <2292861935@qq.com>
2019-10-06 11:26:19 +0800

    修改逻辑

--- src/main/Scala/com/luruixiao/scalaspark/core/examples/WordCount.scala
+++ src/main/Scala/com/luruixiao/scalaspark/core/examples/WordCount.scala
@@ -1,5 +1,6 @@
 package com.luruixiao.scalaspark.core.examples
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -8,18 +9,21 @@ import org.apache.spark.{SparkConf, SparkContext}
 object WordCount {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
-    conf.setMaster("local").setAppName("wordcount")
+    conf.setMaster("local").setAppName("wordCount")
     val sc = new SparkContext(conf)
     val lines = sc.textFile("./data/words")
-    val words = lines.flatMap(line=>{
-      line.split(" ")
-    })
-    val pairWords = words.map(word=>{
-      new Tuple2(word,1)
-    })
-    val reduceResult = pairWords.reduceByKey((v1,v2)=>{v1+v2})
-    val result = reduceResult.sortBy(tp=>{tp._2},false)
-    result.foreach(println)
+//    val words = lines.flatMap(line=>{
+//      line.split(" ")
+//    })
+//    val pairWords = words.map(word=>{
+//      new Tuple2(word,1)
+//    })
+//    val reduceResult = pairWords.reduceByKey((v1,v2)=>{v1+v2})
+//    val result = reduceResult.sortBy(tp=>{tp._2},false)
+//    result.foreach(println)
+//    sc.stop()
+    val data: RDD[(String, Int)] = lines.flatMap(_.split(" ")).map((_,1)).reduceByKey((v1,v2)=>{v1 + v2})
+    data.foreach(println)
     sc.stop()
   }
 }
